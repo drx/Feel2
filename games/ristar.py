0b279c82246d227ef83a9d85e954fd0e6b1716e7
Łukasz Zapart <drx@drx.pl>
2010-09-03 15:35:43 +0200

    Backgrounds.

--- games/ristar.py
+++ games/ristar.py
@@ -45,7 +45,9 @@ class ROM(Project):
         for i in xrange(len(md_palette)/2):
             md_color = Data(md_palette).word(i*2)
             r, g, b = (md_color&0xe), (md_color&0xe0)>>4, (md_color&0xe00) >> 8
-            rgb_color = 0xff000000 + (r << 20) + (g << 12) + (b << 4)
+            rgb_color = (r << 20) + (g << 12) + (b << 4)
+            if i % 16 != 0:
+                rgb_color += 0xff000000
             palette.append(rgb_color)
         return palette
 
@@ -59,12 +61,16 @@ class RistarROM(ROM):
             'palettes': 0x11c1b8,
             'treasure_palette_main': 0xf00c,
             'treasure_palettes_level': 0xf02c,
-            'mappings_256': 0x163716,
-            'mappings_16': 0x11c764,
-            'tiles': 0x129270,
+            'mappings_256_foreground': 0x163716,
+            'mappings_256_background': 0x1853b0,
+            'mappings_16_foreground': 0x11c764,
+            'mappings_16_background': 0x11c814,
+            'mappings_16_background_offset': 0x13ada,
+            'tiles_foreground': 0x129270,
+            'tiles_background': 0x1292c8,
             'objects': 0x5e4,
-            'foreground': 0x188492,
-            'background': 0,
+            'layout_foreground': 0x188492,
+            'layout_background': 0x188552,
             'flora_hack': 0x129320,
             'level_collisions': 0x188baa,
             'collision_array': 0x1e97a,
@@ -133,101 +139,122 @@ class RistarROM(ROM):
             objectset_id = level_id
             collisionset_id = level_id
 
-        tiles_address = self.data.dword(pointers['tiles']+tileset_id*4)
-        tile_data = self.data[tiles_address:]
+
+        def load(address, length=None):
+            if length is None:
+                return self.data[address:]
+            else:
+                return self.data[address:address+length]
+
+        def pointer_array(key, index, alignment=4):
+            return self.data.dword(pointers[key]+index*alignment)
+
+        def pointer_relative_array(key, index, alignment=2):
+            return pointers[key] + self.data.word(pointers[key]+index*2)
+
+        background_offset = self.data.word(pointers['mappings_16_background_offset']+tileset_id*2)
+
+        foreground_tile_data = load(pointer_array('tiles_foreground', tileset_id))
+        background_tile_data = load(pointer_array('tiles_background', tileset_id))
         if tileset_id in (0, 1):
-            flora_hack = star.decompress(self.data[pointers['flora_hack']:])
-            level['tiles'] = flora_hack + star.decompress(tile_data)
+            foreground_tiles = star.decompress(load(pointers['flora_hack'])) + star.decompress(foreground_tile_data)
             if tileset_id == 0:
-                level['tiles'] = '\0'*0x40+level['tiles'] 
+                foreground_tiles = '\0'*0x40+foreground_tiles
+            background_tiles = nemesis.decompress(background_tile_data)
         elif tileset_id == 0x15:
-            level['tiles'] = star.decompress(tile_data)
+            foreground_tiles = star.decompress(foreground_tile_data)
+            background_tiles = star.decompress(background_tile_data)
         else:
-            level['tiles'] = nemesis.decompress(tile_data)
+            foreground_tiles = nemesis.decompress(foreground_tile_data)
+            background_tiles = nemesis.decompress(background_tile_data)
 
-        mappings_256_address = self.data.dword(pointers['mappings_256']+level_id*4)
-        level['mappings_256'] = Data(star.decompress(self.data[mappings_256_address:]))
+        level['tiles'] = foreground_tiles.ljust(background_offset*0x20, '\0') + background_tiles
 
-        mappings_16_address = self.data.dword(pointers['mappings_16']+tileset_id*8)
-        level['mappings_16'] = Data(star.decompress(self.data[mappings_16_address:]))
-
-        objects_address = pointers['objects'] + self.data.word(pointers['objects']+objectset_id*2) + 2
-        level['objects'] = star.decompress(self.data[objects_address:])
-
-        collisions_address = pointers['level_collisions'] + self.data.word(pointers['level_collisions']+objectset_id*2)
-        level['collisions'] = star.decompress(self.data[collisions_address:])
-
-        level['collision_array'] = self.data[pointers['collision_array']:pointers['collision_array']+0x3c0]
+        level['mappings_256_foreground'] = Data(star.decompress(load(pointer_array('mappings_256_foreground', level_id))))
+        level['mappings_256_background'] = Data(star.decompress(load(pointer_array('mappings_256_background', level_id))))
+        level['mappings_16_foreground'] = Data(star.decompress(load(pointer_array('mappings_16_foreground', tileset_id, alignment=8))))
+        mappings_16_background = Data(star.decompress(load(pointer_array('mappings_16_background', tileset_id, alignment=8))))
+        level['mappings_16_background'] = Data(''.join(map(lambda x: struct.pack('>H',x+background_offset), [mappings_16_background.word(i*2) for i in xrange(len(mappings_16_background)/2)])))
+        try:
+            level['objects'] = Data(star.decompress(load(pointer_relative_array('objects', objectset_id)+2)))
+        except:
+            pass
+        level['level_collisions'] = Data(star.decompress(load(pointer_relative_array('level_collisions', objectset_id))))
+        level['collision_array'] = load(pointers['collision_array'], 0x3c0)
 
         if level_id < 0x15:
-            palette_address = pointers['palettes'] + self.data.word(pointers['palettes']+level_id*2)
-            level['palette'] = self.data[palette_address:palette_address+0x80]
+            md_palette = load(pointer_relative_array('palettes', level_id), 0x80)
         else:
-            palette_address = pointers['treasure_palettes_level'] + 0x20*(level_id-0x24)
-            level['palette'] = self.data[palette_address:palette_address+0x40] + '\0'*0x20 + self.data[pointers['treasure_palette']:pointers['treasure_palette']+0x20]
+            md_palette = load(pointer_array('treasure_palettes_level', level_id-0x24, 0x20)) + '\0'*0x20 + load(pointers['treasure_palette_main'], 0x20)
 
-        level['palette'] = RistarROM.palette_md_to_rgb(level['palette'])
+        level['palette'] = RistarROM.palette_md_to_rgb(md_palette)
 
-        address = self.data.dword(pointers['foreground']+level_id*4)
-        x, y = self.data[address]+1, self.data[address+1]+1
-        address += 2
+        def load_layout(address):
+            x, y = self.data[address]+1, self.data[address+1]+1
+            address += 2
        
-        layout = []
-        for cur_y in xrange(y):
-            layout_line = []
-            for cur_x in xrange(x):
-                layout_line.append(self.data[address])
-                address += 1
-            layout.append(layout_line)
+            layout = []
+            for cur_y in xrange(y):
+                layout_line = []
+                for cur_x in xrange(x):
+                    layout_line.append(self.data[address])
+                    address += 1
+                layout.append(layout_line)
 
-        level['foreground'] = {'x': x, 'y': y, 'layout': layout}
+            return {'x': x, 'y': y, 'layout': layout}
 
+        level['foreground'] = load_layout(pointer_array('layout_foreground', level_id))
+        level['background'] = load_layout(pointer_array('layout_background', level_id))
 
         '''Build 16x16 blocks'''
-        blocks_16 = []
-        for i in xrange(len(level['mappings_16'])/8):
-            block = QtGui.QImage(16, 16, QtGui.QImage.Format_ARGB32)
-            for x in range(2):
-                for y in range(2):
-                    flags, tile_id = divmod(level['mappings_16'].word(i*8+y*4+x*2), 0x800)
-                    flip_x, flip_y = flags & 0x1, flags & 0x2
-                    palette_line = (flags & 0xc) >> 2
+        for plane in ('foreground', 'background'):
+            blocks_16 = []
+            for i in xrange(len(level['mappings_16_'+plane])/8):
+                block = QtGui.QImage(16, 16, QtGui.QImage.Format_ARGB32)
+                block.fill(0xff000000)
+                for x in range(2):
+                    for y in range(2):
+                        flags, tile_id = divmod(level['mappings_16_'+plane].word(i*8+y*4+x*2), 0x800)
+                        flip_x, flip_y = flags & 0x1, flags & 0x2
+                        palette_line = (flags & 0xc) >> 2
 
-                    tile_data = level['tiles'][tile_id*0x20:(tile_id+1)*0x20]
-                    tile_i = 0
-                    for tile_byte in tile_data:
-                        tile_y, tile_x = divmod(tile_i, 8)
-                        tile_byte = ord(tile_byte)
-                        for k in (0, 1):
-                            tyle_byte, palette_cell = divmod(tile_byte, 0x10)
-                            color = level['palette'][palette_line*0x10+palette_cell]
-                            set_x, set_y = tile_x+k, tile_y
-                            if flip_x:
-                                set_x = 7-set_x
-                            if flip_y:
-                                set_y = 7-set_y
-                            block.setPixel(x*8+set_x, y*8+set_y, color)
-                        tile_i += 2
+                        tile_data = level['tiles'][tile_id*0x20:(tile_id+1)*0x20]
+                        tile_i = 0
+                        for tile_byte in tile_data:
+                            tile_y, tile_x = divmod(tile_i, 8)
+                            tile_byte = ord(tile_byte)
+                            for k in (0, 1):
+                                tyle_byte, palette_cell = divmod(tile_byte, 0x10)
+                                color = level['palette'][palette_line*0x10+palette_cell]
+                                set_x, set_y = tile_x+k, tile_y
+                                if flip_x:
+                                    set_x = 7-set_x
+                                if flip_y:
+                                    set_y = 7-set_y
+                                block.setPixel(x*8+set_x, y*8+set_y, color)
+                            tile_i += 2
 
-            blocks_16.append(block)
-        level['blocks_16'] = blocks_16
+                blocks_16.append(block)
+            level['blocks_16_'+plane] = blocks_16
 
         '''Build 256x256 blocks'''
-        blocks_256 = []
-        for i in xrange(len(level['mappings_256'])/0x200):
-            block = QtGui.QImage(256, 256, QtGui.QImage.Format_ARGB32)
-            painter = QtGui.QPainter(block)
-            for x in range(16):
-                for y in range(16):
-                    flags, block_16_id = divmod(level['mappings_256'].word(i*0x200+y*0x20+x*2), 0x200)
-                    flip_x, flip_y = bool(flags & 0x2), bool(flags & 0x4)
-                    try:
-                        painter.drawImage(x*16, y*16, level['blocks_16'][block_16_id].mirrored(horizontal = flip_x, vertical = flip_y))
-                    except IndexError:
-                        pass
+        for plane in ('foreground', 'background'):
+            blocks_256 = []
+            for i in xrange(len(level['mappings_256_'+plane])/0x200):
+                block = QtGui.QImage(256, 256, QtGui.QImage.Format_ARGB32)
+                block.fill(0x00000000)
+                painter = QtGui.QPainter(block)
+                for x in range(16):
+                    for y in range(16):
+                        flags, block_16_id = divmod(level['mappings_256_'+plane].word(i*0x200+y*0x20+x*2), 0x200)
+                        flip_x, flip_y = bool(flags & 0x2), bool(flags & 0x4)
+                        try:
+                            painter.drawImage(x*16, y*16, level['blocks_16_'+plane][block_16_id].mirrored(horizontal = flip_x, vertical = flip_y))
+                        except IndexError:
+                            pass
 
-            blocks_256.append(block)
-        level['blocks'] = blocks_256
+                blocks_256.append(block)
+            level['blocks_'+plane] = blocks_256
 
         return level
 
@@ -240,9 +267,14 @@ class Canvas(BaseCanvas):
             return
 
         foreground = self.parent().current_level['foreground']
-        blocks = self.parent().current_level['blocks']
+        background = self.parent().current_level['background']
+        blocks_foreground = self.parent().current_level['blocks_foreground']
+        blocks_background = self.parent().current_level['blocks_background']
 
-        self.max_camera = QtCore.QPoint(256*foreground['x'], 256*foreground['y'])
+        self.background_wrap = True
+        self.block_size = 256
+
+        self.max_camera = QtCore.QPoint(self.block_size*foreground['x'], self.block_size*foreground['y'])
         self.old_camera = QtCore.QPoint(self.camera)
         self.camera += self.delta/self.zoom
         if self.camera.x() < 0:
@@ -268,28 +300,37 @@ class Canvas(BaseCanvas):
 
         if self.reload:
             self.reload = False
-            self.level_image = QtGui.QImage((x_end-x_start)*256, (y_end-y_start)*256, QtGui.QImage.Format_ARGB32)
+            self.level_image = QtGui.QImage((x_end-x_start)*self.block_size, (y_end-y_start)*self.block_size, QtGui.QImage.Format_ARGB32)
             level_painter = QtGui.QPainter(self.level_image)
             level_painter.fillRect(self.level_image.rect(), QtCore.Qt.black)
+
             for y in range(y_start, y_end):
                 for x in range(x_start, x_end):
                     try:
-                        block_id = foreground['layout'][y][x]
+                        foreground_block_id = foreground['layout'][y][x]
                     except IndexError:
-                        pass
-                    if block_id:
+                        foreground_block_id = None
+
+                    background_block_id = background['layout'][y%background['y']][x%background['x']]
+                    if background_block_id:
+                        level_painter.drawImage((x-x_start)*self.block_size, (y-y_start)*self.block_size, blocks_background[background_block_id-1])
+                    if foreground_block_id:
                         try:
-                            level_painter.drawImage((x-x_start)*256, (y-y_start)*256, blocks[block_id-1])
+                            level_painter.drawImage((x-x_start)*self.block_size, (y-y_start)*self.block_size, blocks_foreground[foreground_block_id-1])
                         except IndexError:
                             pass
+        
 
         source_rect = self.level_image.rect()
-        source_rect.setX((self.camera.x()&0xff)+256)
+        source_rect.setX((self.camera.x()&0xff)+self.block_size)
         source_rect.setWidth(self.width()/self.zoom)
-        source_rect.setY((self.camera.y()&0xff)+256)
+        source_rect.setY((self.camera.y()&0xff)+self.block_size)
         source_rect.setHeight(self.height()/self.zoom)
         painter.drawImage(self.rect(), self.level_image, source_rect)
 
+
+    def resizeEvent(self, event):
+        self.reload = True
 
     def mouseMoveEvent(self, event):
         from PyQt4 import QtGui, QtCore
@@ -396,20 +437,21 @@ class LoadLevels(QtCore.QThread):
         rom.load('./roms/Ristar - The Shooting Star (J) [!].bin')
 
         self.started.emit(len(rom.levels))
+        self.progress.emit(0)
 
         levels = {}
         thumbnails = {}
         i = 0
         for level_id in rom.levels:
-            if level_id > 3:
+            if level_id > 0x15:
                 break
             try:
                 levels[level_id] = rom.load_level(level_id)
-            except Exception as e:
+            except object as e:
                 print 'Could not load level {id} ({e})'.format(id=level_id, e=e)
 
             # select block with biggest entropy for thumbnail
-            blocks_entropy = map(self.entropy, levels[level_id]['blocks'])
+            blocks_entropy = map(self.entropy, levels[level_id]['blocks_foreground'])
             thumbnails[level_id] = blocks_entropy.index(max(blocks_entropy))
 
             i += 1
@@ -418,12 +460,14 @@ class LoadLevels(QtCore.QThread):
         self.loaded.emit(levels, thumbnails, rom)
 
 
-class LevelSelector(BaseLevelSelector):
-    def __init__(self, level_names):
-        super(LevelSelector, self).__init__()
+class BlockSelector(QtGui.QWidget):
+    def __init__(self, blocks, block_size=256):
+        super(BlockSelector, self).__init__()
 
-        self.level_names = level_names
-        self.current_thumb = None
+        self.current_block = None
+        self.block_size = block_size
+        self.blocks = blocks.values()
+        self.left = 0
 
         self.setMouseTracking(True)
 
@@ -439,15 +483,13 @@ class LevelSelector(BaseLevelSelector):
         painter = QtGui.QPainter(self)
         painter.fillRect(self.rect(), QtCore.Qt.black)
 
-        for level_id, thumbnail_id in self.editor.thumbnails.items():
-            thumbnail = QtGui.QImage(self.editor.levels[level_id]['blocks'][thumbnail_id])
-
-            if self.current_thumb == level_id:
+        for block_i, block in enumerate(self.blocks[self.left:]):
+            thumbnail = QtGui.QImage(block) 
+            if self.current_block == block_i:
                 thumb_border = 0
             else:
                 thumb_border = 5
-    
-                alpha = QtGui.QImage(256, 256, QtGui.QImage.Format_RGB32)
+                alpha = QtGui.QImage(self.block_size, self.block_size, QtGui.QImage.Format_RGB32)
                 alpha.fill(0x808080)
                 thumbnail.setAlphaChannel(alpha)
 
@@ -456,11 +498,22 @@ class LevelSelector(BaseLevelSelector):
             i += 1
 
     def mouseMoveEvent(self, event):
-        self.current_thumb = (event.x()-10)/(self.height()-20)
+        self.current_block = (event.x()-10)/(self.height()-20)
 
     def mousePressEvent(self, event):
-        if self.current_thumb:
-            self.editor.current_level = self.editor.levels[self.current_thumb]
+        self.left += 1
+
+class LevelSelector(BlockSelector):
+    def __init__(self, level_names, editor):
+        self.editor = editor
+        blocks = dict((level_id, self.editor.levels[level_id]['blocks_foreground'][thumbnail_id]) for (level_id, thumbnail_id) in self.editor.thumbnails.items())
+        super(LevelSelector, self).__init__(blocks)
+
+        self.level_names = level_names
+
+    def mousePressEvent(self, event):
+        if self.current_block is not None:
+            self.editor.current_level = self.editor.levels[self.current_block]
             self.editor.canvas.reload = True
 
 
@@ -488,10 +541,11 @@ class Pane(BasePane):
         self.parent().current_level = levels[0]
         self.parent().level_loaded = True
 
-        self.level_selector = LevelSelector(project.levels)
-        self.level_selector.editor = self.parent()
+        self.level_selector = LevelSelector(project.levels, editor=self.parent())
+        self.foreground_selector = BlockSelector(dict(enumerate(levels[0]['blocks_foreground'])))
 
         self.addTab(self.level_selector, 'Levels')
+        self.addTab(self.foreground_selector, 'Foreground')
 
     
 class Editor(BaseEditor):
